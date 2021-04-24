5028fb10b6d98374331dc22d929b4f8af170b9a5
Damian Bhatia <32986767+DamianBhatia@users.noreply.github.com>
2021-04-24 16:00:06 -0400

    Update Solution.py

--- Project-Euler/Problem 1/Solution.py
+++ Project-Euler/Problem 1/Solution.py
@@ -1,1 +1,29 @@
+import math
 
+''' Want all multiples of 3 and 5 so the numbers will look like 3*i and 5*i where
+    i is a natural number. Multiples of both 3 and 5 will look like 15*i. To find the 
+    sum under 1000 we want to add up all the multiples of 3 and all multiples of 5
+    and then subtract all multiples of 15 since they will be overcounted. Can achieve this
+    by doing simple summation
+'''
+
+
+''' Get sum of all multiples of 3 and 5 that are smaller than n. '''
+def getSum(n):
+    # number of multiples of 3 under n
+    n1 = (n-1) // 3 
+    # number of multiples of 5 under n
+    n2 = (n-1) // 5
+    # number of multiples of 15 under n
+    n3 = (n-1) // 15
+    
+    # summation formula
+    return 3 * sum_n(n1) + 5 *sum_n(n2) - 15 * sum_n(n3)
+    
+    
+''' Summation formula for i = 1 to n. '''
+def sum_n(n):
+    return (n * (n+1) / 2)
+    
+    
+print(getSum(1000))
