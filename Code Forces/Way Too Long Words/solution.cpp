5b35b4b428c1cf9ac355ebf9a13fbc5f6e16c229
Damian Bhatia <32986767+DamianBhatia@users.noreply.github.com>
2021-05-01 13:12:59 -0400

    Create solution.cpp

---
+++ Code Forces/Way Too Long Words/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	
+	string word;
+	getline(cin, word);
+
+	for(int i = 0; i < num; ++i) {
+		getline(cin, word);
+		if(word.length() > 10) {
+			cout << word.at(0) << word.length() - 2 << word.at(word.length() - 1) << endl;
+		} else {
+			cout << word << endl;
+		}
+	}
+
+  return 0;
+}
